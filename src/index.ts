75b0be9c34e7b0f9303b76c0e41de376be9596c0
Ashwin Mahadevan <mahadevan@ashwinm.com>
2024-02-27 15:40:06 -0800

    Types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,49 @@
 import { OpenAI } from "openai";
 import type { ChatCompletionMessageParam } from "openai/resources/chat/completions";
 import { z } from "zod";
-import { zodToOpenAISchema } from "./openai-schema";
+import { zodToOpenAISchema } from "./openai-schema.js";
+
+type ChatSystemMessage = {
+	/** A role of "system" tells the LLM that a developer wrote the message. */
+	role: "system";
+	name?: string;
+	content: string;
+};
+
+type ChatUserMessage = {
+	/** A role of "user" tells the LLM that a user wrote the message. */
+	role: "user";
+	name?: string;
+	content: string;
+};
+
+type ChatAssistantMessage = {
+	/** A role of "assistant" tells the LLM that an LLM wrote the message (in many cases itself, but in some cases a different LLM). */
+	role: "assistant";
+	name?: string;
+	content?: string | null;
+	calls?: Array<{
+		id: string;
+		name: string;
+		arguments: string;
+	}>;
+};
+
+type ChatToolMessage = {
+	role: "tool";
+	id: string;
+	content: string;
+};
+
+type ChatMessage =
+	| ChatSystemMessage
+	| ChatUserMessage
+	| ChatAssistantMessage
+	| ChatToolMessage;
 
 type ChatOptions = {
 	/** An array of messages in the conversation so-far. */
-	messages: Array<ChatCompletionMessageParam>;
+	messages: Array<ChatMessage>;
 };
 
 type ChatOptionsWithTool<TSchema extends z.ZodType> = ChatOptions & {
@@ -21,14 +59,14 @@ type ChatOptionsWithTool<TSchema extends z.ZodType> = ChatOptions & {
 	};
 };
 
-type ResponseMessage = { role: string; content: string | null };
+type ResponseMessage = { role: "assistant"; content: string | null };
 
 type ResponseMessageWithCalls<TSchema extends z.ZodType> = ResponseMessage & {
 	calls: Array<{ id: string; arguments: z.output<TSchema> }>;
 };
 
 export class ZGPT {
-	private openAI: OpenAI;
+	private readonly openAI: OpenAI;
 
 	/**
 	 * Creates a new ZGPT instance given an OpenAI API key.
@@ -63,7 +101,49 @@ export class ZGPT {
 	): Promise<ResponseMessage | ResponseMessageWithCalls<z.ZodType>> {
 		const completion = await this.openAI.chat.completions.create({
 			model: "gpt-4-turbo-preview",
-			messages: options.messages,
+			messages: options.messages.map((message): ChatCompletionMessageParam => {
+				switch (message.role) {
+					case "assistant":
+						return {
+							role: "assistant",
+							name: message.name,
+							content: message.content,
+							tool_calls: message.calls?.map((call) => ({
+								id: call.id,
+								type: "function",
+								function: {
+									name: call.name,
+									arguments: call.arguments,
+								},
+							})),
+						};
+
+					case "system":
+						return {
+							role: "system",
+							content: message.content,
+							name: message.name,
+						};
+
+					case "tool":
+						return {
+							role: "tool",
+							tool_call_id: message.id,
+							content: message.content,
+						};
+
+					case "user":
+						return {
+							role: "user",
+							content: message.content,
+							name: message.name,
+						};
+					default:
+						// @ts-expect-error: In type-safe code, this is unreachable.
+						throw new Error(`Unexpected message role: ${message.role}`);
+				}
+			}),
+
 			tools:
 				"tool" in options
 					? [
